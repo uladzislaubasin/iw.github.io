2ebcf3eb878aa0262d97843a89768686b005a6ff
Vlad Basin <vladbasin@gmail.com>
2020-06-11 02:52:58 +0300

    Push first version

---
+++ precache-manifest.586eb74930a6a0712894c72598b1d56e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dfc21ae95e5bc3cdeed8fc5e058071f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ae969debe0ba30584eb",
+    "url": "/static/js/2.a20bd88b.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/2.a20bd88b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f704b699a07a6ae24b04",
+    "url": "/static/js/main.60077dd0.chunk.js"
+  },
+  {
+    "revision": "157fd12af11676160fe2",
+    "url": "/static/js/runtime-main.29ce19bc.js"
+  }
+]);
